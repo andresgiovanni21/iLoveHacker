351b5a34c267ac3992d20c1eb878a37650c38ab2
andresgiovanni21 <andresgiovanni21@hotmail.com>
2021-06-08 15:43:44 -0500

    primerCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Intro1Component } from './intro1/intro1.component';
+import { Intro2Component } from './intro2/intro2.component';
+import { Intro3Component } from './intro3/intro3.component';
+import { MalwareComponent } from './malware/malware.component';
+import { PassComponent } from './pass/pass.component';
+import { TarjetaComponent } from './tarjeta/tarjeta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Intro1Component,
+    Intro2Component,
+    Intro3Component,
+    MalwareComponent,
+    PassComponent,
+    TarjetaComponent
   ],
   imports: [
     BrowserModule
